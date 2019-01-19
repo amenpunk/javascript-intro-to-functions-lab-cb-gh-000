e9b1ef473514ce9588d3be087554d8c14d4921dc
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-19 19:12:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function whisper(string) {
 /*  logShout(string) calls console.log() its one argument in all caps:
     ReferenceError: logShout is not defined */
 function logShout(string) {
-  console.log() string.toUpperCase
+  console.log() 
 }
 
 /*  logWhisper(string) calls console.log() its one argument in all lowercase:
